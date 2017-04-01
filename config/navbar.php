4c204750ee8a190df1ec8a2da9ca57e7af324f45
Peder Tornberg <peder.tornberg@gmail.com>
2017-04-01 22:06:51 +0200

    Added config and view for the navbar

---
+++ config/navbar.php
@@ -0,0 +1,20 @@
+<?php
+$navbar = [
+    "config" => [
+        "navbar-class" => "navbar2"
+    ],
+    "items" => [
+        "me" => [
+            "text" => "Start",
+            "route" => "",
+        ],
+        "about" => [
+            "text" => "Om",
+            "route" => "about",
+        ],
+        "report" => [
+            "text" => "Redovisning",
+            "route" => "report",
+        ],
+    ]
+];
--- view/take1/navbar.php
+++ view/take1/navbar.php
@@ -1,15 +1,48 @@
 <?php
-$urlHome  = $app->url->create("");
-$urlAbout = $app->url->create("about");
-$urlReport = $app->url->create("report");
+require(ANAX_INSTALL_PATH . "/config/navbar.php");
+// var_dump($navbar);
 
+
+/**
+ * Check if item matches current route
+ * @method isActiveLink
+ * @param  [array]       $item [array item from navbar config]
+ * @return string            [result with class name for active]
+ */
+function isActiveLink($item)
+{
+    global $app;
+
+    $currentUrl = $app->request->getRoute();
+    if ($currentUrl === $item["route"]) {
+        return ' class="active"';
+    }
+}
+
+
+/**
+ * Function to generate list items för the navbar
+ * @method generateNavbarList
+ * @param  [Array]             $items [navbar links]
+ * @return [html list-items]                    [li-items in html]
+ */
+function generateNavbarList($items)
+{
+    global $app;
+
+    // var_dump($items);
+    $output = "";
+    foreach ($items as $item) {
+        $output .= '<li' . isActiveLink($item) . '><a href="' . $app->url->create($item["route"]) .
+                   '" title="' . $item['text'] . '">' . $item['text'] . '</a></li>' . "\n";
+    }
+    return $output;
+}
 ?>
-<nav class="navbar2" role="navigation">
+<nav class="<?= $navbar["config"]["navbar-class"] ?>" role="navigation">
     <div class="rm-navbar-top rm-navbar">
         <ul class="rm-default rm-desktop">
-            <li><a href="<?= $urlHome ?>">Home</a></li>
-            <li><a href="<?= $urlAbout ?>">About</a></li>
-            <li><a href="<?= $urlReport ?>">Report</a></li>
+            <?= generateNavbarList($navbar["items"]) ?>
         </ul>
     </div>
 </nav>
