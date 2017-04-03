7fc886c233a2586c9b2b364b0b5a381c9fab4f7e
Peder Tornberg <peder.tornberg@gmail.com>
2017-04-03 21:47:01 +0200

    Added missing link to guess game.

--- view/take1/about.php
+++ view/take1/about.php
@@ -7,7 +7,8 @@
                 <p>Denna sida är en del i kursen OOPHP på BTH</p>
                 <p>Här kan ni se repot på github:
                 <p><a href="https://github.com/ptorn/anax-lite" title="Anax-Lite" target="_blank">Anax-Lite</a></p>
-                <p><a href="status">Länk till systemstatus</a></p>
+                <p><a href="status" title="Systemstatus">Länk till systemstatus</a></p>
+                <p><a href="../../kmom01/guess" title="Guess spelet">Guess spelet</a></p>
                 <p align="center"><img src="../htdocs/image/php.png?w=300" alt="php"></p>
             </main>
         </div>
